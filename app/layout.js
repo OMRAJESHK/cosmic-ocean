6b397313efe6cded3f44ac06b2b70200c9dbf2ae
om rajesh <omrajeshk6021@gmail.com>
2023-08-31 14:44:45 +0530

    single file testing app/layout.js

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
       </Head>
       <body>
         <CosmicNavbar />
+        {/*  */}
         <main>{children}</main>
       </body>
     </html>
